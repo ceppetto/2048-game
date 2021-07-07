1a791b319b27068826225e833c7fdd8dd4ede325
ceppetto <pcy07111@gmail.com>
2021-07-07 18:45:07 +0900

    Semi version

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 let board = new Array(4);
+let totalScore = 0;
+const scoreLabel = document.getElementById('score');
 
 // set board value to zero
 function setBoard() {
@@ -38,15 +40,67 @@ function randBoxPicker() {
 
 function paintBoard() {
     let isFull = checkFull();
-    if(isFull) randBoxPicker();
+    if(!isFull) randBoxPicker();
+    if(isFull) endGame();
+
+    
+    scoreLabel.innerText = `Total Score: ${totalScore}`;
 
     for(let i = 0; i < 4; i ++) {
         let pickedTr = document.getElementById('tr-' + i);
         for(let j = 0; j < 4; j ++) {
             pickedTr.children[j].innerText = board[i][j];
+            if(board[i][j] === 0) {
+                pickedTr.children[j].style.backgroundColor = "#CDC1B4";
+                pickedTr.children[j].style.color = "#7b766e";
+                pickedTr.children[j].innerText = '';
+            }
+            if(board[i][j] === 2) {
+                pickedTr.children[j].style.backgroundColor = "#EEE4DA";
+                pickedTr.children[j].style.color = "#7b766e";
+            }
+            if(board[i][j] === 4) {
+                pickedTr.children[j].style.backgroundColor = "#EDE0C8";
+                pickedTr.children[j].style.color = "#7b766e";
+            }
+            if(board[i][j] === 8) {
+                pickedTr.children[j].style.backgroundColor = "#F2B179";
+                pickedTr.children[j].style.color = "white";
+            }
+            if(board[i][j] === 16) {
+                pickedTr.children[j].style.backgroundColor = "#F59563";
+                pickedTr.children[j].style.color = "white";
+            }
+            if(board[i][j] === 32) {
+                pickedTr.children[j].style.backgroundColor = "#F67B61";
+                pickedTr.children[j].style.color = "white";
+            }
+            if(board[i][j] === 64) {
+                pickedTr.children[j].style.backgroundColor = "#F65E3B";
+                pickedTr.children[j].style.color = "white";
+            }
+            if(board[i][j] === 128) {
+                pickedTr.children[j].style.backgroundColor = "#EDCF72";
+                pickedTr.children[j].style.color = "white";
+            }
+            if(board[i][j] === 256) {
+                pickedTr.children[j].style.backgroundColor = "#EDCF72";
+                pickedTr.children[j].style.color = "white";
+            }
+            if(board[i][j] === 512) {
+                pickedTr.children[j].style.backgroundColor = "#EDCF72";
+                pickedTr.children[j].style.color = "white";
+            }
+            if(board[i][j] === 1024) {
+                pickedTr.children[j].style.backgroundColor = "#EDCF72";
+                pickedTr.children[j].style.color = "white";
+            }
+            if(board[i][j] === 2048) {
+                pickedTr.children[j].style.backgroundColor = "#EDC22E";
+                pickedTr.children[j].style.color = "white";
+            }
         }
     }
-    
 }
 
 function startGame() {
@@ -55,16 +109,15 @@ function startGame() {
 }
 
 function checkFull() {
-    let flag = 0;
+    let flag = 1;
     for(let i = 0; i < 4; i ++) {
         for(let j = 0; j < 4; j ++) {
             if(board[i][j] === 0) {
-                flag = 1;
+                flag = 0;
                 return flag;
             }
         }
     }
-
     return flag;
 }
 
@@ -74,7 +127,7 @@ function endGame() {
 
 // keyboard controls
 function pressUp() {
-    for(let loop = 0; loop < 3; loop ++) {
+    for(let loop = 0; loop < 2; loop ++) {
         for(let i = 0; i < 3; i ++) {
             for(let j = 0; j < 4; j ++) {
                 if(board[i][j] === 0) {
@@ -82,6 +135,19 @@ function pressUp() {
                     board[i+1][j] = 0;
                 } else if (board[i][j] === board[i+1][j]) {
                     board[i][j] += board[i+1][j];
+                    board[i+1][j] = 0;
+                    totalScore += board[i][j];
+                }
+            }
+        }
+    }
+    
+
+    for(let loop = 0; loop < 2; loop ++) {
+        for(let i = 0; i < 3; i ++) {
+            for(let j = 0; j < 4; j ++) {
+                if(board[i][j] === 0) {
+                    board[i][j] = board[i+1][j];
                     board[i+1][j] = 0;
                 }
             }
@@ -92,7 +158,7 @@ function pressUp() {
 }
 
 function pressDown() {
-    for(let loop = 0; loop < 3; loop ++) {
+    for(let loop = 0; loop < 2; loop ++) {
         for(let i = 3; i > 0; i --) {
             for(let j = 0; j < 4; j ++) {
                 if(board[i][j] === 0) {
@@ -101,7 +167,19 @@ function pressDown() {
                 } else if (board[i][j] === board[i-1][j]) {
                     board[i][j] += board[i-1][j];
                     board[i-1][j] = 0;
+                    totalScore += board[i][j];
                 }
+            }
+        }
+    }
+
+    for(let loop = 0; loop < 2; loop ++) {
+        for(let i = 3; i > 0; i --) {
+            for(let j = 0; j < 4; j ++) {
+                if(board[i][j] === 0) {
+                    board[i][j] = board[i-1][j];
+                    board[i-1][j] = 0;
+                } 
             }
         }
     }
@@ -110,7 +188,7 @@ function pressDown() {
 }
 
 function pressRight() {
-    for(let loop = 0; loop < 3; loop ++) {
+    for(let loop = 0; loop < 2; loop ++) {
         for(let i = 0; i < 4; i ++) {
             for(let j = 3; j > 0; j --) {
                 if(board[i][j] === 0) {
@@ -118,6 +196,18 @@ function pressRight() {
                     board[i][j-1] = 0;
                 } else if (board[i][j] === board[i][j-1]) {
                     board[i][j] += board[i][j-1];
+                    board[i][j-1] = 0;
+                    totalScore += board[i][j];
+                }
+            }
+        }
+    }
+    
+    for(let loop = 0; loop < 2; loop ++) {
+        for(let i = 0; i < 4; i ++) {
+            for(let j = 3; j > 0; j --) {
+                if(board[i][j] === 0) {
+                    board[i][j] = board[i][j-1];
                     board[i][j-1] = 0;
                 }
             }
@@ -128,7 +218,7 @@ function pressRight() {
 }
 
 function pressLeft() {
-    for(let loop = 0; loop < 3; loop ++) {
+    for(let loop = 0; loop < 2; loop ++) {
         for(let i = 0; i < 4; i ++) {
             for(let j = 0; j < 3; j ++) {
                 if(board[i][j] === 0) {
@@ -136,6 +226,17 @@ function pressLeft() {
                     board[i][j+1] = 0;
                 } else if (board[i][j] === board[i][j+1]) {
                     board[i][j] += board[i][j+1];
+                    board[i][j+1] = 0;
+                    totalScore += board[i][j];
+                }
+            }
+        }
+    }
+    for(let loop = 0; loop < 2; loop ++) {
+        for(let i = 0; i < 4; i ++) {
+            for(let j = 0; j < 3; j ++) {
+                if(board[i][j] === 0) {
+                    board[i][j] = board[i][j+1];
                     board[i][j+1] = 0;
                 }
             }
